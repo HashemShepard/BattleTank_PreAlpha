97db10677f4b8a8c1bef77728009bbea2275e6a5
HashemShepard <element_hero4@hotmail.com>
2018-08-27 23:17:55 +0200

    BT_005 Vid24

---
+++ BT_Pre_Alpha/Source/BT_Pre_Alpha/Public/TankPlayerController.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Tank.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BT_PRE_ALPHA_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	ATank* GetControlledTank() const;
+
+
+private:
+	
+	
+};
